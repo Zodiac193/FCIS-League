8f6da0c7f7f6de303cfcc194a8904b88ec064a4c
Mohamed Refaat <medom193@gmail.com>
2023-08-25 18:06:31 +0300

    Update Program.cs

--- FCIS League/Program.cs
+++ FCIS League/Program.cs
@@ -1,7 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿//An application to manage and collect every information about a football league like teams, players, matches, and results
+//The League Contains 20 Teams
+//Each Team Has 34 Players
+//Each Player Has: Name - Birth Date - Nationality
+//Each Team Playes Against the Other 19 Teams ONCE in the Season
+//Matches Are Every Friday: 4:00PM - 6:00PM - 8:00PM
+//Each Friday Contains 10 Matches
+//Each Time Slot Contains 3 Matches Except the Last Contains 4 Matches
+//The App Will Generate randomly all matches scheduled before the season starts.
+//The App Will Save all the Data in Files
+//No one can edit the data from the files after the season start
+
